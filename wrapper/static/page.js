4855054e6743be6361d248c1167c3b037f7a29c3
Daza <aigobuss@gmail.com>
2022-04-09 20:58:48 +0000

    fix css

--- wrapper/static/page.js
+++ wrapper/static/page.js
@@ -136,8 +136,8 @@ module.exports = function (req, res, url) {
 				}
 			}
 		</script>
-		<link rel="stylesheet" type="text/css" href="/pages/css/modern-normalize.css">
-		<link rel="stylesheet" type="text/css" href="/pages/css/global.css">
+		<link rel="stylesheet" type="text/css" href="../css/modern-normalize.css" />
+		<link rel="stylesheet" type="text/css" href="../css/global.css" />
 		<style>
 			body {
 				background: #eee;
