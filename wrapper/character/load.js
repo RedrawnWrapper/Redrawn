1cd44843b1bd6c7749c2e2013f4345de3890fd21
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-04-10 12:16:38 -0400

    Update load.js

--- wrapper/character/load.js
+++ wrapper/character/load.js
@@ -14,34 +14,29 @@ module.exports = function (req, res) {
 				.catch(e => { res.statusCode = 404, res.end(e) })
 			return true;
 		}
+
 		case 'POST': {
-			switch (req.url) {
-				case '/goapi/getCcCharCompositionXml/': {
-					loadPost(req, res).then(async data => {
-						console.log("Loading character: "+data.assetId||data.original_asset_id)
-						res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-						process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-						character.load(data.assetId || data.original_asset_id).then(v => { res.statusCode = 200, res.end(0 + v) })
-						//.catch(e => { res.statusCode = 404, res.end(1 + e) })
-						//why send a 404 when you can watch benson on youtube
-							.catch(() => character.load('a-327068826').then(v => {
+			if (req.url != '/goapi/getCcCharCompositionXml/') return;
+			loadPost(req, res).then(async data => {
+				console.log("Loading character: "+data.assetId||data.original_asset_id)
+				res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+				process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+				character.load(data.assetId || data.original_asset_id)
+					.then(v => { res.statusCode = 200, res.end(0 + v) })
+					//.catch(e => { res.statusCode = 404, res.end(1 + e) })
+					//why send a 404 when you can watch benson on youtube
+					.catch(
+						() => character.load('a-327068826')
+						.then(v => {
 							console.log("Couldn't find that character, but it's okay, we loaded Benson instead."),
-								res.statusCode = 200, res.end(0 + v)
-						})).catch(e => {
-							console.log("But nobody came."),
-								res.statusCode = 404, res.end(1 + e)
-						});
+							res.statusCode = 200, res.end(0 + v)
+						})
+					).catch(e => {
+						console.log("But nobody came."),
+						res.statusCode = 404, res.end(1 + e)
 					});
-					break;
-				}
-				case '/goapi/deleteAsset/': {
-					loadPost(req, res).then(async data => {
-						console.log("Deleting character: "+data.assetId||data.original_asset_id)
-						character.delete(data.assetId || data.original_asset_id)
-					});
-					break;
-				}
-			}
+			});
+			return true;
 		}
 		default: return;
 	}
