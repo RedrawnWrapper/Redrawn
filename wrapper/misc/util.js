d17a2963da46344c1e2e003bc7c1e10435eeaf31
JC Video <51718271+JCVideo@users.noreply.github.com>
2022-04-17 22:35:08 +0100

    Update util.js

--- wrapper/misc/util.js
+++ wrapper/misc/util.js
@@ -1,5 +1,5 @@
 module.exports = {
-	xmlFail(message = "There's a voice error!.") {
+	xmlFail(message = "There's a voice error!") {
 		return `<error><code>ERR_ASSET_404</code><message>${message}</message><text></text></error>`;
 	},
 };
