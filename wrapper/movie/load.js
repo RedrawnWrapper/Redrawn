f3a1a6088f694e425cafec6d96527f8e45c10ae2
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-12 21:48:15 -0400

    Error Catching for if anything goes wrong

--- wrapper/movie/load.js
+++ wrapper/movie/load.js
@@ -10,11 +10,13 @@ module.exports = function (req, res, url) {
 			var id = match[1], ext = match[2];
 			switch (ext) {
 				case 'zip':
+					console.log(`Fetching Movie Zip: ${id}`);
 					res.setHeader('Content-Type', 'application/zip');
 					movie.loadZip(id).then(v => { res.statusCode = 200, res.end(v) })
 						.catch(e => { console.log(`Error Fetching Movie Zip: ${id}`); })
 					break;
 				default:
+					console.log(`Fetching Movie Xml: ${id}`);
 					res.setHeader('Content-Type', 'text/xml');
 					movie.loadXml(id).then(v => { res.statusCode = 200, res.end(v) })
 						.catch(e => { console.log(`Error Fetching Movie Xml: ${id}`); })
@@ -27,22 +29,22 @@ module.exports = function (req, res, url) {
 			switch (url.pathname) {
 				case '/goapi/getMovie/': {
 					movieId = url.query.movieId;
+					console.log(`Loading Movie: ${movieId}`);
 					res.setHeader('Content-Type', 'application/zip');
 					process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
 					movie.loadZip(movieId).then(b =>
-				                res.end(Buffer.concat([base, b]))).catch(e => console.log(`Error Loading Movie: ${movieId}`));
+				                res.end(Buffer.concat([base, b]))).catch(e => { console.log(`Error Loading Movie: ${movieId}`); });
 					return true;
 				}
 				case '/ajax/deleteMovie/': {
 					movieId = url.query.movieId;
+					console.log(`Deleting Movie: ${movieId}`);
 					res.setHeader('Content-Type', 'application/zip');
 					process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 					
-					movie.delete(movieId).then(b =>
-				                console.log(`Deleting Movie: ${movieId}`)).catch(e => { console.log(`Error Deleting Movie: ${movieId}`); });
-                                        movie.deleteThumb(movieId).then(b =>
-				                console.log(`Deleting Movie Thumb: ${movieId}`)).catch(e => { console.log(`Error Deleting Movie Thumb: ${movieId}`); }); 
+					movie.delete(movieId).catch(e => { console.log(`Error Deleting Movie: ${movieId}`); });
+                                        movie.deleteThumb(movieId).catch(e => { console.log(`Error Deleting Movie Thumb: ${movieId}`); }); 
 					return true;
 				}
 			}
