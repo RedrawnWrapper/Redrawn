4bf05114745b3f44782fed149cdf197b2f48bb45
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-24 21:37:42 -0400

    Update upload.js

--- wrapper/starter/upload.js
+++ wrapper/starter/upload.js
@@ -3,18 +3,37 @@ const parse = require('../data/parse');
 const fUtil = require('../fileUtil');
 const fs = require('fs');
 
-module.exports = function (req, res, url) {
-	if (req.method != 'POST' || url.path != '/upload_starter') return;
-	new formidable.IncomingForm().parse(req, (e, f, files) => {
-		const path = files.import.path, buffer = fs.readFileSync(path);
-		const numId = fUtil.getNextFileId('starter-', '.xml');
-		parse.unpackStarterXml(buffer, numId);
-		fs.unlinkSync(path);
+module.exports = function (req, res, url, window) {
+	if (req.method != 'POST') {
+		switch (url.pathname) {
+			case '/upload_starter': {
+				new formidable.IncomingForm().parse(req, (e, f, files) => {
+					const path = files.import.path, buffer = fs.readFileSync(path);
+					const numId = fUtil.getNextFileId('starter-', '.xml');
+					parse.unpackStarterXml(buffer, numId);
+					fs.unlinkSync(path);
 
-		res.statusCode = 302;
-		const url = `https://localhost:7462/starterUploaded?returnMessage=Your Starter Has Been Uploaded. Id: s-${numId}`;
-		res.setHeader('Location', url);
-		res.end();
-	});
-	return true;
+					res.statusCode = 302;
+					const url = `https://localhost:7462/starterUploaded?returnMessage=Your Starter Has Been Uploaded. Id: s-${numId}`;
+					res.setHeader('Location', url);
+					res.end();
+				});
+				return true;
+			}
+			case '/upload_starter_swf': {
+				new formidable.IncomingForm().parse(req, (e, f, files) => {
+					const path = files.import.path, buffer = fs.readFileSync(path);
+					const numId = fUtil.getNextFileId('starter-', '.xml');
+					parse.unpackStarterXml(buffer, numId);
+					fs.unlinkSync(path);
+
+					res.statusCode = 302;
+					window.alert(`Your Starter Has Been Uploaded. Id: s-${numId}`);
+					res.end();
+				});
+				return true;
+			}
+			default: return;
+		}
+	}
 }
