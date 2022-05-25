5ce90f6b04e44c31407926f5ebb1de8366f77cb4
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-24 21:48:03 -0400

    Update upload.js

--- wrapper/movie/upload.js
+++ wrapper/movie/upload.js
@@ -3,18 +3,37 @@ const parse = require('../data/parse');
 const fUtil = require('../fileUtil');
 const fs = require('fs');
 
-module.exports = function (req, res, url) {
-	if (req.method != 'POST' || url.path != '/upload_movie') return;
-	new formidable.IncomingForm().parse(req, (e, f, files) => {
-		const path = files.import.path, buffer = fs.readFileSync(path);
-		const numId = fUtil.getNextFileId('movie-', '.xml');
-		parse.unpackXml(buffer, numId);
-		fs.unlinkSync(path);
+module.exports = function (req, res, url, window) {
+	if (req.method != 'POST') {
+		switch (url.pathname) {
+			case '/upload_movie': {
+				new formidable.IncomingForm().parse(req, (e, f, files) => {
+					const path = files.import.path, buffer = fs.readFileSync(path);
+					const numId = fUtil.getNextFileId('movie-', '.xml');
+					parse.unpackXml(buffer, numId);
+					fs.unlinkSync(path);
 
-		res.statusCode = 302;
-		const url = `/go_full?movieId=m-${numId}`;
-		res.setHeader('Location', url);
-		res.end();
-	});
-	return true;
+					res.statusCode = 302;
+					const url = `/go_full?movieId=m-${numId}`;
+					res.setHeader('Location', url);
+					res.end();
+				});
+				return true;
+			}
+			case '/upload_movie_swf': {
+				new formidable.IncomingForm().parse(req, (e, f, files) => {
+					const path = files.import.path, buffer = fs.readFileSync(path);
+					const numId = fUtil.getNextFileId('movie-', '.xml');
+					parse.unpackXml(buffer, numId);
+					fs.unlinkSync(path);
+
+					res.statusCode = 302;
+					window.alert(`Your Movie Has Been Uploaded. Id: m-${numId}`);
+					res.end();
+				});
+				return true;
+			}
+			default: return;
+		}
+	}
 }
