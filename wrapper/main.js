7e787f10b7bc1ec3906044fa3ad0660a17075d4f
Kia <kia@anistick.com>
2022-12-25 13:37:36 -0600

    Update main.js

--- wrapper/main.js
+++ wrapper/main.js
@@ -1,1 +1,17 @@
-require('./server');
+const env = Object.assign(process.env,require("./env"));
+
+if (env.LVM_ENV == "NON_SERVER")
+{
+  // If this is Redrawn Offline / GoAnimator Environment
+  console.log("Environment: Offline");
+  require("./server");
+} else if (env.LVM_ENV == "SERVER")
+{
+  // If this is Redrawn Online / Server Environment
+  console.log("Environment: Online");
+  require("./server");
+} else {
+  // Otherwise
+  console.log("ERROR: Invalid environment");
+  process.exit(1);
+}
