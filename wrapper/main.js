3ee00bd17654082d554c79c4c61093c9c5ae45c3
Indy <41709262+c4artisan@users.noreply.github.com>
2022-12-25 18:13:56 -0800

    Revert "Update main.js"
    This reverts commit 7e787f10b7bc1ec3906044fa3ad0660a17075d4f.

--- wrapper/main.js
+++ wrapper/main.js
@@ -1,17 +1,1 @@
-const env = Object.assign(process.env,require("./env"));
-
-if (env.LVM_ENV == "NON_SERVER")
-{
-  // If this is Redrawn Offline / GoAnimator Environment
-  console.log("Environment: Offline");
-  require("./server");
-} else if (env.LVM_ENV == "SERVER")
-{
-  // If this is Redrawn Online / Server Environment
-  console.log("Environment: Online");
-  require("./server");
-} else {
-  // Otherwise
-  console.log("ERROR: Invalid environment");
-  process.exit(1);
-}
+require('./server');
